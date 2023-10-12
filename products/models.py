b44acafe45abaa29a9c4503a1abf2596b99f1d1b
Shohelrana63 <shohelrana.beg@gmail.com>
2023-10-12 15:22:59 +0600

    models new update

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+class category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class product_all(models.Model):
+    name = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='product_picture/')
+    old_price = models.PositiveIntegerField()
+    new_price = models.PositiveIntegerField()
+    description = models.TextField(max_length=250)
+    category = models.ForeignKey(category, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
